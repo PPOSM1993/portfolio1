35d0e3682838f77d1f0b1e05c98eddec5309c03f
pedroosorio <pedro.osorio04@inacapmail.cl>
2025-03-10 12:58:02 -0300

    creacion about y projects

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
 import {Navbar} from './components/Navbar'
 import { MobileMenu } from './components/MobileMenu'
-import {Home} from './components/sections/Home'
+import { Home } from './components/sections/Home'
+import { About } from './components/sections/About'
+import Projects from './components/sections/Projects'
 import './index.css'
 import { useState } from 'react'
 function App() {
@@ -19,6 +21,8 @@ function App() {
         <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
         <Home />
+        <About />
+        <Projects />
       </div>
     </>
   )
